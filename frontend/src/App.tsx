21849161ec6a11afc780d9e10b0ab7ab299452b3
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-22 22:02:30 +0100

    Update App function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,17 @@ import './App.css'
 import {LoginForm} from "@/app/features/auth/components/login-form.tsx";
 import { ThemeProvider } from "@/components/theme-provider"
 import Layout from '@/app/features/shared/components/layout/layout';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoutes from '@/routes/app-routes';
 
 function App() {
   return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-
-            <Layout>
-                <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-                    <div className="w-full max-w-sm">
-                        <LoginForm />
-                    </div>
-                </div>
-            </Layout>
+            <BrowserRouter>
+                <Layout>
+                    <AppRoutes />
+                </Layout>
+            </BrowserRouter>
         </ThemeProvider>
   )
 }
