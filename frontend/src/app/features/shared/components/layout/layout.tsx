5af497e8777611562a1fcc9b9f6a1e07e3545374
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-23 15:54:54 +0100

    Update layout

--- frontend/src/app/features/shared/components/layout/layout.tsx
+++ frontend/src/app/features/shared/components/layout/layout.tsx
@@ -1,17 +1,21 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar.tsx"
 import { AppSidebar } from "@/app/features/shared/components/navigation/sidebars/app-sidebar.tsx";
 import { Outlet } from "react-router-dom";
+import { AppTopbar } from "../navigation/topbars/app-topbar";
 //import AppTopbar from "@/components/shared/navigation/sidebars/app-topbar.tsx";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main className={"flex-1"}>
-                <SidebarTrigger />
-                {children}
-                <Outlet />
-            </main>
-        </SidebarProvider>
+        <>
+            <AppTopbar />
+                <SidebarProvider>
+                <AppSidebar />
+                <main className={"flex-1"}>
+                    <SidebarTrigger />
+                    {children}
+                    <Outlet />
+                </main>
+            </SidebarProvider>
+        </>
     )
 }
