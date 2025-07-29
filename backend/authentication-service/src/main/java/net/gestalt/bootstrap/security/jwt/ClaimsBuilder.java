4cf56545885041216b04f5a50a2b46487c967afc
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-29 23:13:43 +0100

    Add roles, groups, organizations to jwt claims

--- backend/authentication-service/src/main/java/net/gestalt/bootstrap/security/jwt/ClaimsBuilder.java
+++ backend/authentication-service/src/main/java/net/gestalt/bootstrap/security/jwt/ClaimsBuilder.java
@@ -3,10 +3,23 @@ package net.gestalt.bootstrap.security.jwt;
 import io.smallrye.jwt.build.Jwt;
 import io.smallrye.jwt.build.JwtClaimsBuilder;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import net.gestalt.core.model.Group;
+import net.gestalt.core.model.Organization;
+import net.gestalt.core.model.Role;
 import net.gestalt.core.model.User;
+
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class ClaimsBuilder {
+
+    private final TokenProvider tokenProvider;
+
+    @Inject
+    public ClaimsBuilder(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
 
     public JwtClaimsBuilder fromUser(User user) {
         var claimsBuilder = Jwt.claims()
@@ -14,12 +27,21 @@ public class ClaimsBuilder {
                 .upn(user.getEmail())
                 .subject(user.getEmail())
                 .claim("userId", user.getId())
-                .claim("role", user.getRole())
-                .groups("groups", user.getGroups().getName());
-
-        if (user.getOrganization() != null) {
-            claimsBuilder.claim("organizationId", user.getOrganization().getId());
-        }
+                .claim("role", user.getRole()
+                        .stream()
+                        .map(Role::getName)
+                        .collect(Collectors.toSet())
+                )
+                .groups(user.getGroups()
+                                .stream()
+                                .map(Group::getName)
+                                .collect(Collectors.toSet())
+                )
+                .claim("organizations", user.getOrganization()
+                        .stream()
+                        .map(Organization::getName)
+                        .collect(Collectors.toSet())
+                );
 
         return claimsBuilder;
     }
