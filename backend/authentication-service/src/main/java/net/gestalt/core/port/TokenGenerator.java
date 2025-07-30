404af53796690b62b3e984815811d32b1a557c0c
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-30 20:41:20 +0100

    Change arch from hexagonal to clean

--- backend/authentication-service/src/main/java/net/gestalt/core/port/TokenGenerator.java
+++
@@ -1,7 +0,0 @@
-package net.gestalt.core.port;
-
-import net.gestalt.core.model.User;
-
-public interface TokenGenerator {
-    String generate(User user);
-}
--- backend/authentication-service/src/native-test/java/net/gestalt/LoginResourceIT.java
+++
@@ -1,8 +0,0 @@
-package net.gestalt;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class LoginResourceIT extends LoginResourceTest {
-    // Execute the same tests but in packaged mode.
-}
