cfff6beb2c84d639d349dfc1ba2ebb1285140d40
OliverKeefe <oliverkeefe@tutanota.com>
2025-07-31 22:29:28 +0100

    Add authentication state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
+import { useEffect, useState } from "react";
 import { ThemeProvider } from "@/components/theme-provider"
 import Layout from '@/app/features/shared/components/layout/layout';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from '@/routes/app-routes';
+import keycloak, { initKeycloak } from "@/security/auth/keycloak/keycloak";
 
 function App() {
-  return (
+    const [isKeycloakReady, setIsKeycloakReady] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        initKeycloak().then((kc) => {
+            if (kc) {
+                console.log('Token:', kc.token);
+                console.log('User:', kc.tokenParsed?.preferred_username);
+                setIsAuthenticated(true);
+            }
+            setIsKeycloakReady(true);
+        });
+    }, []);
+
+    if (!isKeycloakReady) {
+        return <div className="p-8 text-center text-lg">🔐 Initializing authentication...</div>
+    }
+
+    return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <BrowserRouter>
                 <Layout>
@@ -13,7 +33,7 @@ function App() {
                 </Layout>
             </BrowserRouter>
         </ThemeProvider>
-  )
+    );
 }
 
-export default App
+export default App;
