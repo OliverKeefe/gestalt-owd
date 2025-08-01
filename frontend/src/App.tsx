19642df26c45c6280bdf4b126a660364ee695a33
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-01 16:23:46 +0100

    Keycloak double init bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,12 @@ import { ThemeProvider } from "@/components/theme-provider"
 import Layout from '@/app/features/shared/components/layout/layout';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from '@/routes/app-routes';
-import keycloak, { initKeycloak } from "@/security/auth/keycloak/keycloak";
 
-function App() {
-    const [isKeycloakReady, setIsKeycloakReady] = useState(false);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+interface AppProps {
+    isAuthenticated: boolean;
+}
 
-    useEffect(() => {
-        initKeycloak().then((kc) => {
-            if (kc) {
-                console.log('Token:', kc.token);
-                console.log('User:', kc.tokenParsed?.preferred_username);
-                setIsAuthenticated(true);
-            }
-            setIsKeycloakReady(true);
-        });
-    }, []);
-
-    if (!isKeycloakReady) {
-        return <div className="p-8 text-center text-lg">🔐 Initializing authentication...</div>
-    }
-
+function App({ isAuthenticated }: AppProps) {
     return (
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <BrowserRouter>
