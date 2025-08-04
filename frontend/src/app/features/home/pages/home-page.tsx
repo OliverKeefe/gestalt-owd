d0b2da66f2ff6fe24b447057e53867d6395f7892
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-04 16:46:52 +0100

    Use Container component over div

--- frontend/src/app/features/home/pages/home-page.tsx
+++ frontend/src/app/features/home/pages/home-page.tsx
@@ -2,24 +2,20 @@ import {SectionCards} from "@/app/features/shared/components/cards/section-cards
 import {Table} from "@/components/ui/table.tsx";
 import {FileTable} from "@/app/features/shared/components/tables/file-table.tsx";
 import { Container } from "../../shared/components/layout/container";
+import { FileCardCarousel } from "../../shared/components/carousel/file-card-carousel";
 
 
 export function Home() {
     return (
         <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    <h1 className="text-2xl font-semibold p-6 m-1">Actions</h1>
-                    <SectionCards />
-                </div>
-                <div>
-                    <h1 className="text-2xl font-semibold p-6 m-1">All files</h1>
                     <Container>
+                        <h1 className="text-2xl font-semibold p-6 m-1">Folders</h1>
+                        <FileCardCarousel />
+                    </Container>
+                    <Container>
+                        <h1 className="text-2xl font-semibold p-6 m-1">All files</h1>
                         <FileTable />
                     </Container>
-
-                </div>
             </div>
-        </div>
     );
 }
