0b841440c7ec8167c35cfc815738f2322fd792a1
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-04 16:47:49 +0100

    Implement verifyUsername method

--- backend/authentication-service/src/main/java/net/gestalt/app/service/user/VerifyCredentials.java
+++ backend/authentication-service/src/main/java/net/gestalt/app/service/user/VerifyCredentials.java
@@ -3,6 +3,9 @@ package net.gestalt.app.service.user;
 import jakarta.inject.Inject;
 import net.gestalt.api.dto.login.LoginRequest;
 import net.gestalt.app.service.password.PasswordService;
+import net.gestalt.data.entity.User;
+
+import java.util.Objects;
 
 public class VerifyCredentials {
 
@@ -13,7 +16,8 @@ public class VerifyCredentials {
     PasswordService passwordService;
 
     public boolean verifyUsername(String username) {
-        return userService.findByUsername(username);
+        User findUser = userService.findByUsername(username);
+        return Objects.equals(findUser.getUsername(), username);
     }
 
     public boolean verifyPassword(String password) {
