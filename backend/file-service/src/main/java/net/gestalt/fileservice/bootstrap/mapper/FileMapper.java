07e6d07caaefdc811bd384d4673e194a93b5637d
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-17 16:29:05 +0100

    Add toModel method

--- backend/file-service/src/main/java/net/gestalt/fileservice/bootstrap/mapper/FileMapper.java
+++ backend/file-service/src/main/java/net/gestalt/fileservice/bootstrap/mapper/FileMapper.java
@@ -1,4 +1,27 @@
 package net.gestalt.fileservice.bootstrap.mapper;
 
+import net.gestalt.fileservice.adapter.api.dto.request.FileRequest;
+import net.gestalt.fileservice.core.model.FileModel;
+
 public class FileMapper {
+
+    public FileMapper() {}
+
+    public FileModel toModel(FileRequest fileRequest) {
+        FileModel fileModel = new FileModel(
+                fileRequest.id(),
+                fileRequest.orgId(),
+                fileRequest.ownerId(),
+                fileRequest.storageType(),
+                fileRequest.storageKey(),
+                fileRequest.fileSize(),
+                fileRequest.checksum(),
+                fileRequest.createdAt(),
+                fileRequest.lastModifiedAt()
+        );
+
+        return fileModel;
+    }
+
+    public FileModel toModel(FileEntity fileEntity) {}
 }
