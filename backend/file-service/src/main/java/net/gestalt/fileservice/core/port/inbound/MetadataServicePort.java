b941022cc36936c9764a78c187a457174020a07f
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-17 21:33:32 +0100

    Add getFile method to MetadataServicePort

--- backend/file-service/src/main/java/net/gestalt/fileservice/core/port/inbound/MetadataServicePort.java
+++ backend/file-service/src/main/java/net/gestalt/fileservice/core/port/inbound/MetadataServicePort.java
@@ -1,4 +1,8 @@
 package net.gestalt.fileservice.core.port.inbound;
 
+import net.gestalt.fileservice.adapter.api.dto.response.FileResponse;
+import net.gestalt.fileservice.core.model.FileModel;
+
 public interface MetadataServicePort {
+    FileResponse getFile(FileModel fileModel);
 }
