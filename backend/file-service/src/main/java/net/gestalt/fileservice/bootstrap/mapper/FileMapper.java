f18318841b41a090e18c38c68dbd12d31f687f03
OliverKeefe <oliverkeefe@tutanota.com>
2025-08-18 21:23:37 +0100

    Switch from manual to MapStruct mappers

--- backend/file-service/src/main/java/net/gestalt/fileservice/bootstrap/mapper/FileMapper.java
+++ backend/file-service/src/main/java/net/gestalt/fileservice/bootstrap/mapper/FileMapper.java
@@ -1,42 +1,17 @@
 package net.gestalt.fileservice.bootstrap.mapper;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import net.gestalt.fileservice.adapter.api.dto.request.FileRequest;
 import net.gestalt.fileservice.adapter.persistence.entity.FileEntity;
 import net.gestalt.fileservice.core.model.FileModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
 
-public class FileMapper {
+@Mapper(componentModel = "cdi")
+public interface FileMapper {
 
-    public FileMapper() {}
+    FileModel toModel(FileRequest fileRequest);
 
-    public FileModel toModel(FileRequest fileRequest) {
-        FileModel fileModel = new FileModel(
-                fileRequest.id(),
-                fileRequest.orgId(),
-                fileRequest.ownerId(),
-                fileRequest.storageType(),
-                fileRequest.storageKey(),
-                fileRequest.fileSize(),
-                fileRequest.checksum(),
-                fileRequest.createdAt(),
-                fileRequest.lastModifiedAt()
-        );
-
-        return fileModel;
-    }
-
-    public FileModel toModel(FileEntity fileEntity) {
-        FileModel fileModel = new FileModel(
-                fileEntity.id,
-                fileEntity.orgId,
-                fileEntity.ownerId,
-                fileEntity.storageType,
-                fileEntity.storageKey,
-                fileEntity.fileSize,
-                fileEntity.checksum,
-                fileEntity.createdAt,
-                fileEntity.lastModifiedAt
-        );
-
-        return fileModel;
-    }
+    FileModel toModel(FileEntity fileEntity);
 }
