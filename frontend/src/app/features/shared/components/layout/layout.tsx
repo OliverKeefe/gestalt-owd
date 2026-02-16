4e2046c1df87a2e0a9c1247201b376a5de1a64a4
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-02-16 17:41:17 +0000

    Add SecondarySidebar to Layout

--- frontend/src/app/features/shared/components/layout/layout.tsx
+++ frontend/src/app/features/shared/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/app/features/shared/components/navigation/sidebars/app-sidebar"
 import { Outlet } from "react-router-dom"
 import { AppTopbar } from "../navigation/topbars/app-topbar"
+import {SecondarySidebar} from "@/app/features/shared/components/navigation/sidebars/secondary-sidebar.tsx";
 
 interface LayoutProps {
     children?: React.ReactNode;
@@ -10,16 +11,18 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
     return (
         <SidebarProvider>
-            <div className="flex min-w-screen">
+            <div className="flex w-screen overflow-x-hidden ">
                 <AppSidebar />
-                <div className="flex flex-1 flex-col">
+                <SecondarySidebar />
+                <div className="pl-16 flex flex-1 min-w-0 flex-col">
                     <AppTopbar>
                         <SidebarTrigger />
                     </AppTopbar>
-                    <main className="flex-1">
+                    <main className="flex-1 min-w-0 overflow-x-hidden theme-scroll">
                         {children}
                         <Outlet />
                     </main>
+
                 </div>
             </div>
         </SidebarProvider>
