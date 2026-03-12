c40f9648ec188cae1efea5a58bed990892514bcb
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-03-12 18:53:48 +0000

    Add TestService_GetAll

--- backend/src/usecase/files/service_test.go
+++ backend/src/usecase/files/service_test.go
@@ -80,3 +80,25 @@ func TestService_Upload(t *testing.T) {
 	}
 
 }
+
+func TestService_GetAll(t *testing.T) {
+	var cur = MetadataCursor{
+		ModifiedAt: time.Now(),
+		ID:         uuid.New(),
+	}
+
+	var req = GetAllMetadataRequest{
+		UserID: uuid.New(),
+		Cursor: &cur,
+		Limit:  20,
+	}
+
+	md, err := svc.FindAllMetadata(context.Background(), req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if reflect.TypeOf(md) != reflect.TypeOf([]MetaData{}) {
+		t.Fatalf("expected slice of Metadata, got: %T", md)
+	}
+}
