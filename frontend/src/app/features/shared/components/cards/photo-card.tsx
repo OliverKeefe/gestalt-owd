ac7888acb8dabadd931dc3f7e04b272588ebc0cf
OliverKeefe <104454089+OliverKeefe@users.noreply.github.com>
2026-08-30 22:40:57 +0100

    Remove dead code from PhotoCard

--- frontend/src/app/features/shared/components/cards/photo-card.tsx
+++ frontend/src/app/features/shared/components/cards/photo-card.tsx
@@ -1,13 +1,10 @@
 import {Card, CardContent} from "@/components/ui/card";
-import { React } from 'react';
 
 export function PhotoCard() {
-    const placeholder: string = "https://placehold.co/600x400/000000/FFFFFF/png";
-
     return (
         <Card>
             <CardContent>
-                <img src={"https://placehold.co/600x400/000000/FFFFFF/png" || placeholder} />
+                <img src={"https://placehold.co/600x400/000000/FFFFFF/png"} />
                 <p>Photo title</p>
             </CardContent>
         </Card>
